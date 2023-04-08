a488177a75c2157f5255988d79746788e2c9a63f
zhandos200103201 <saparbayevzh03@gmail.com>
2023-04-09 00:49:31 +0600

    this week to our project was added filtering by book rating and user can give additional rating for books. Book rating will be chaged every time when user give a rating

--- pkg/controllers/bookController.go
+++ pkg/controllers/bookController.go
@@ -4,6 +4,7 @@ import (
 	"Test2/initializers"
 	"Test2/pkg/models"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
@@ -92,11 +93,11 @@ func DeleteBook(c *gin.Context) {
 	})
 }
 
-func FilteredBooks(c *gin.Context) {
+func PriceFiltering(c *gin.Context) {
 	from := c.Query("from")
 	to := c.Query("to")
 	var books []models.Book
-	result := initializers.GetDB().Where("price >= ?", from).Where("price <= ?", to).Find(&books)
+	result := initializers.GetDB().Where("price >= ?", from).Where("price <= ?", to).Order("price").Find(&books)
 
 	if result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -107,5 +108,65 @@ func FilteredBooks(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"result": books,
+	})
+}
+
+func RatingFiltering(c *gin.Context) {
+	rating := c.Query("rating")
+	var books []models.Book
+	result := initializers.GetDB().Where("rating >= ?", rating).Order("rating").Find(&books)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Error": "failed to get books by rating",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": books,
+	})
+}
+
+func GiveRating(c *gin.Context) {
+	var body struct {
+		Email     string
+		Password  string
+		Book_name string
+		Rating    float32
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	user := models.User{}
+	initializers.GetDB().First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Wrong gmail or password",
+		})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(body.Password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Wrong password. Try again",
+		})
+		return
+	}
+
+	var target models.Book
+	initializers.GetDB().Find(&target, "title=?", body.Book_name)
+	avg := (body.Rating + target.Rating) / 2
+	if body.Rating <= 5.0 {
+		initializers.GetDB().Model(&target).Update("Rating", avg)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"Change book rating": avg,
 	})
 }
--- pkg/routes/router.go
+++ pkg/routes/router.go
@@ -9,10 +9,12 @@ import (
 var Router = func(router *gin.Engine) {
 	router.GET("/books", controllers.GetBooks)
 	router.POST("/books", controllers.CreateBook)
+	router.POST("/give_a_rating", controllers.GiveRating)
 	router.GET("/books/:id", controllers.GetBook)
 	router.PUT("/books/:id", controllers.EditBook)
 	router.DELETE("/books/:id", controllers.DeleteBook)
-	router.GET("/filter", controllers.FilteredBooks)
+	router.GET("/filter_by_prices", controllers.PriceFiltering)
+	router.GET("/filter_by_rating", controllers.RatingFiltering)
 	router.POST("/signup", controllers.Signup)
 	router.POST("/login", controllers.Login)
 	router.GET("/logout", controllers.SignOut)
